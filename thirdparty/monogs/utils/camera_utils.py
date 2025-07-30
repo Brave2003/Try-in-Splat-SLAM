5390662abe7ed6b1faa6c9e211265b9935835771
enterfutures <760296832@qq.com>
2025-07-30 10:24:33 +0800

    Update camera_utils.py

--- thirdparty/monogs/utils/camera_utils.py
+++ thirdparty/monogs/utils/camera_utils.py
@@ -270,8 +270,20 @@ class Camera(nn.Module):
         # Select the keyframes with percentage of points inside the image > 0
         selected_keyframe_list = [keyframe_dict['id']
                                   for keyframe_dict in list_keyframe if keyframe_dict['percent_inside'] > 0.0]
-        selected_keyframe_list = list(np.random.permutation(
-            np.array(selected_keyframe_list))[:8 - pose_window])
+        win = 8 - pose_window
+        if len(selected_keyframe_list) >= win:
+            selected_keyframe_list = list(np.random.permutation(
+                np.array(selected_keyframe_list))[:win])
+        elif len(selected_keyframe_list) < win:
+        # Need to add extra frames to make it 5
+            remaining_needed = win - len(selected_keyframe_list)
+        # Get all available frames (including those with percent_inside <= 0)
+            all_frames = [keyframe_dict['id'] for keyframe_dict in list_keyframe]
+        # Exclude already selected frames
+            available_frames = [f for f in all_frames if f not in selected_keyframe_list]
+        # Take the top remaining frames (since list_keyframe is already sorted)
+            extra_frames = available_frames[:remaining_needed]
+            selected_keyframe_list.extend(extra_frames)
 
         return selected_keyframe_list
 
